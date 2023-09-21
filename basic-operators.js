8eb50b42799196b238beca072b646679345bac9d
JM Baldonado <jmbaldonado.xurpas@gmail.com>
2023-09-21 19:54:24 +0800

    TOPIC 01: introduce arithmetic operations in JS

---
+++ basic-operators.js
@@ -0,0 +1,24 @@
+console.log('----- Addition -----')
+const sum = 101 + 214;
+console.log('sum: ', sum);
+
+const sumInvalid = 101 + '214';
+console.log('sumInvalid: ', sumInvalid);
+console.log(typeof sumInvalid)
+
+console.log('----- Subtraction -----')
+
+const difference = 4344 - 4343;
+console.log('difference: ', difference);
+
+console.log('----- Multiplication -----')
+const product = 12 * 2;
+console.log('product: ', product);
+
+console.log('----- Division -----')
+const quotient = 94 / 3;
+console.log('quotient: ', quotient);
+
+console.log('----- Modulo -----')
+const remainder = 94 % 3;
+console.log('remainder: ', remainder);
