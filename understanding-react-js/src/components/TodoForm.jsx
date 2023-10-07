649d8c07fb82404e9c2c90839699356263e4f442
JM Baldonado <jmbaldonado.xurpas@gmail.com>
2023-10-07 16:56:05 +0800

    introduce state

--- understanding-react-js/src/components/TodoForm.jsx
+++ understanding-react-js/src/components/TodoForm.jsx
@@ -1,8 +1,13 @@
-function TodoForm() {
+function TodoForm(props) {
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.addTodo()
+    }
     return (
-        <form className={'todo-form'}>
+        <form className={'todo-form'} onSubmit={handleSubmit}>
             <input type="text" placeholder={'What is the task today?'} className={'todo-input'} />
-            <button className={'todo-btn'}>Add Task</button>
+            <button className={'todo-btn'} type="submit">Add Task</button>
         </form>
     )
 }
--- understanding-react-js/src/components/TodoWrapper.jsx
+++ understanding-react-js/src/components/TodoWrapper.jsx
@@ -1,12 +1,26 @@
 
+import React, { useState } from 'react'
 import TodoForm from './TodoForm';
 import Todo from './Todo';
 
 function TodoWrapper() {
+    const [todo, setTodo] = useState([]);
+
+    function addTodo(todo) {
+        console.log('im clicked!')
+        // setTodo((prevState) => {
+        //     const newTodo = {
+        //         task: todo,
+        //         completed: false
+        //     }
+        //     return [...prevState, newTodo]
+        // })
+    }
+
     return (
         <div className={'todo-wrapper'}>
             <h1>Get Things Done</h1>
-            <TodoForm />
+            <TodoForm addTodo={addTodo} />
             <Todo />
         </div>
     )
